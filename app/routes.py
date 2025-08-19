0133947e2bfa79ab33ffde547a8474e7dd503998
kimaxim <maximkim.29.06.2k5@gmail.com>
2025-08-19 22:01:43 +0500

    first chapter

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import web_app
 
 @web_app.route("/")
 @web_app.route("/index")
 def index():
-    return "This is the main page"
+    user = {"login": "maxim"}
+    posts = [
+        {
+            'author': {'login': 'max'},
+            'message': 'Overkills is fine'
+        },
+        {
+            'author': {'login': 'kim'},
+            'message': 'Stay down till you come up'
+        }
+    ]
+    return render_template("index.html", title="Home", username=user, posts=posts)
